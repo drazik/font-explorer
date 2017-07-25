563d1e8c52efd6c1650c43c23e7d04e88a42d543
Cyrille Perois <cperois@outlook.com>
2017-07-25 15:16:53 +0200

    font-size slider styles

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,5 +1,36 @@
 const React = require("react");
 const PropTypes = require("prop-types");
+const styled = require("styled-components").default;
+
+const SliderContainer = styled.div`
+  display: inline-flex;
+
+  align-items: center;
+`;
+
+const SliderBoundary = styled.span`
+  font-family: serif;
+  font-weight: bold;
+`;
+
+const SliderBoundaryLower = SliderBoundary.extend`
+  font-size: 0.8rem;
+`;
+
+const SliderBoundaryUpper = SliderBoundary.extend`
+  font-size: 1.3rem;
+`;
+
+const SliderInput = styled.input.attrs({
+  type: "range",
+  min: props => props.min || 1,
+  max: props => props.max || 4,
+  step: props => props.step || 1,
+  onChange: props => props.onChange,
+})`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
 
 const Slider = ({
   min = 1,
@@ -8,14 +39,21 @@ const Slider = ({
   value = 1,
   onChange
 }) => (
-  <input
-    type="range"
-    min={min}
-    max={max}
-    step={step}
-    value={value}
-    onChange={onChange}
-  />
+  <SliderContainer>
+    <SliderBoundaryLower>
+      A
+    </SliderBoundaryLower>
+    <SliderInput
+      min={min}
+      max={max}
+      step={step}
+      value={value}
+      onChange={onChange}
+    />
+    <SliderBoundaryUpper>
+      A
+    </SliderBoundaryUpper>
+  </SliderContainer>
 );
 
 Slider.propTypes = {
